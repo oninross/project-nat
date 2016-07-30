21dd6d3c895733f2feeb1035915b621caed88923
Nino Ross Rodriguez <oninross@gmail.com>
2016-07-30 13:54:05 +0800

    Removed lost index file Updated favicon

--- _templates/includes/header.php
+++ _templates/includes/header.php
@@ -11,29 +11,54 @@
         <meta name="description" content="">
         <title>Project N.A.T.</title>
 
-        <link rel="apple-touch-icon" sizes="57x57" href="/assets/project-nat/images/favicon/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="/assets/project-nat/images/favicon/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="/assets/project-nat/images/favicon/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="/assets/project-nat/images/favicon/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="/assets/project-nat/images/favicon/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="/assets/project-nat/images/favicon/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="/assets/project-nat/images/favicon/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="/assets/project-nat/images/favicon/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="/assets/project-nat/images/favicon/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="/assets/project-nat/images/favicon/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/assets/project-nat/images/favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="/assets/project-nat/images/favicon/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/assets/project-nat/images/favicon/favicon-16x16.png">
+        <meta name="robots" content="follow">
+        <meta name="author" content="Nino Ross Rodriguez">
+        <meta name="copyright" content="infinite imaginations 2016">
+        <meta name="description" content="One page application that aggigates news to your preference and tells the weather">
+        <meta name="keywords" content="news aggrigator, one page, weather" />
 
-        <!-- build:css /assets/project-nat/css/main.min.css -->
-        <link href="/assets/project-nat/css/main.css" rel="stylesheet">
+        <meta property="og:title" content="Project N.A.T.">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="">
+        <meta property="og:image" content="">
+        <meta property="og:description" content="One page application that aggigates news to your preference and tells the weather">
+        <meta property="og:site_name" content="">
+
+        <meta name="twitter:card" content="summary"/>
+        <meta name="twitter:creator" content="Nino Ross Rodriguez"/>
+        <meta name="twitter:url" content=""/>
+
+        <meta name="twitter:title" content="Project N.A.T."/>
+        <meta name="twitter:description" content="One page application that aggigates news to your preference and tells the weather"/>
+        <meta name="twitter:image" content="" />
+
+        <link rel="apple-touch-icon" sizes="57x57" href="assets/project-nat/images/favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="assets/project-nat/images/favicon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="assets/project-nat/images/favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="assets/project-nat/images/favicon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="assets/project-nat/images/favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="assets/project-nat/images/favicon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="assets/project-nat/images/favicon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="assets/project-nat/images/favicon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/project-nat/images/favicon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="assets/project-nat/images/favicon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/project-nat/images/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="assets/project-nat/images/favicon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/project-nat/images/favicon/favicon-16x16.png">
+        <link rel="manifest" href="assets/project-nat/images/favicon/manifest.json">
+        <meta name="msapplication-TileColor" content="#54606e">
+        <meta name="msapplication-TileImage" content="assets/project-nat/images/favicon/ms-icon-144x144.png">
+        <meta name="theme-color" content="#54606e">
+
+        <!-- build:css assets/project-nat/css/main.min.css -->
+        <link href="assets/project-nat/css/main.css" rel="stylesheet">
         <!-- /build -->
 
-        <!-- build:css /assets/project-nat/css/responsive.min.css -->
-        <link href="/assets/project-nat/css/responsive.css" rel="stylesheet">
+        <!-- build:css assets/project-nat/css/responsive.min.css -->
+        <link href="assets/project-nat/css/responsive.css" rel="stylesheet">
         <!-- /build -->
 
-        <script src="/assets/project-nat/js/vendor/modernizr.js"></script>
+        <script src="assets/project-nat/js/vendor/modernizr.js"></script>
     </head>
     <body>
         <div class="dashboard">
